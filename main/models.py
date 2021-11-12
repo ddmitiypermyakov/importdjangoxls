12af7e9092677cd8c2e21906cd8a7188040991bd
Dmitry <ddmitiy@mail.ru>
2021-11-12 13:41:33 +0300

    Create models and module export/import

--- main/models.py
+++ main/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Products(models.Model):
+
+    name=models.CharField('Наименование',max_length=255,db_index=True)
+    description=models.TextField('Описание',blank=True,help_text='Опишите товар')
+    price = models.DecimalField('Цена',max_digits=6,decimal_places=2,default=0.00)
+    date_published=models.DateTimeField('Дата публикации',auto_now_add=True)
+    available = models.BooleanField('Наличие',default=False)
+
+    def __str__(self):
+        return self.name
+
+
--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,11 @@
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from .views import upload_file
 urlpatterns = [
 
     path('', upload_file,name='upload_file'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
